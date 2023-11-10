a6fe44e0a66ce76ce999f6af99fa399ee07a29b3
Mohammad Mahdi Samei <9259samei@gmail.com>
2023-11-10 17:52:27 +0000

    feature: FastAPI route added + tests

--- src/frameworks/api/entry_point.py
+++ src/frameworks/api/entry_point.py
@@ -1,5 +1,5 @@
 import multiprocessing
-
+from typing import Any, Annotated, Union
 import uvicorn
 from fastapi import (
     FastAPI,
@@ -11,18 +11,28 @@ from fastapi import (
     Request,
     Form,
 )
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, OAuth2
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
-from dependencies import container # noqa
-from src.adapters.authentication_provider.authentication import (
-    Authenticate,
-    ServiceAuthorize,
-)
+from dependencies import container  # noqa
 from src.config import service_config as service_config
 from src.frameworks.api.configs.fastapi_doc import (
     fastapi_information,
     fastapi_tags_metadata,
 )
 from src.utils.basic_logger import simple_logger as logger
+import jwt
+from src.frameworks.api.schemas.user import UserSignup
+from src.frameworks.api.schemas.message import CreateMessage, FetchAllMessages
+from src.entities import User, RoleType, Interaction, Message
+from src.use_cases import UserUseCase, InteractionUseCase, MessageUseCase
+from src.use_cases.exceptions import (
+    UserNotAuthorized,
+    InteractionNotFound,
+    UserNotFound,
+)
+
+import json
 
 
 class FastAPIConfig:
@@ -34,18 +44,29 @@ class FastAPIConfig:
         self.tags_metadata = fastapi_tags_metadata
 
 
+class Auth:
+    def __init__(self):
+        self.oauth2_scheme = OAuth2PasswordBearer(
+            tokenUrl="/login",
+        )
+
+
 class APIEndpoint:
     def __init__(
         self,
-        auth: Authenticate,
-        authorize: ServiceAuthorize,
+        auth: Auth,
         configs: FastAPIConfig,
         logger: logger,
+        user_use_case: UserUseCase,
+        interaction_use_case: InteractionUseCase,
+        message_use_case: MessageUseCase,
     ) -> None:
         self.auth = auth
-        self.authorize = authorize
         self.configs = configs
         self.logger = logger
+        self.user_use_case = user_use_case
+        self.interaction_use_case = interaction_use_case
+        self.message_use_case = message_use_case
         self.rest_api_app = None
         self.create_rest_api_app(self.configs.information, self.configs.tags_metadata)
         self.create_rest_api_route()
@@ -73,9 +94,6 @@ class APIEndpoint:
             tags=["Main"],
             responses={
                 status.HTTP_200_OK: {"description": "Success"},
-                status.HTTP_401_UNAUTHORIZED: {
-                    "description": "Error in authenticating user due to invalid token."
-                },
             },
         )
         async def main(request: Request):
@@ -83,11 +101,77 @@ class APIEndpoint:
             return JSONResponse(
                 status_code=status.HTTP_200_OK, content={"message": "Success"}
             )
-        
+
+        @self.rest_api_app.post(
+            "/signup",
+            tags=["User"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+            },
+        )
+        async def signup(form_data: UserSignup):
+            """Create a new user."""
+            response = await self.user_use_case.add_new_user(
+                User(
+                    id=None,
+                    name=form_data.name,
+                    email=form_data.email,
+                    password=form_data.password,
+                )
+            )
+            if response is None:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"message": "Failed to create user"},
+                )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK, content={"message": "Success, user created. Please login to continue."}
+            )
+
+        @self.rest_api_app.post(
+            "/login",
+            tags=["User"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+            },
+        )
+        async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+            try:
+                response = await self.user_use_case.user_login(
+                    form_data.username, form_data.password
+                )
+            except UserNotFound as e:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"User not found, please sign up first. Error: {e}",
+                )
+            except UserNotAuthorized as e:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail=f"User not authorized. Error: {e}",
+                )
+            except Exception as e:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Internal server error. Error: {e}",
+                )
+            user_data = {
+                "id": response.id,
+                "name": response.name,
+                "email": response.email,
+                "is_verified": response.is_verified,
+            }
+            # create a token for the user with jwt
+            token = jwt.encode(
+                user_data, service_config.JWT_SECRET_KEY, algorithm="HS256"
+            )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK, content={"access_token": token, "token_type": "bearer"}
+            )
 
         @self.rest_api_app.get(
             "/service",
-            tags=["Main"],
+            tags=["AuthCheck"],
             responses={
                 status.HTTP_200_OK: {"description": "Success"},
                 status.HTTP_401_UNAUTHORIZED: {
@@ -97,9 +181,171 @@ class APIEndpoint:
         )
         async def service(
             token: str = Depends(self.auth.oauth2_scheme),
-            has_permission: bool = Depends(self.authorize),
         ):
             """Check the Status of the Service."""
             return JSONResponse(
                 status_code=status.HTTP_200_OK, content={"message": "Success"}
+            )
+
+        @self.rest_api_app.post(
+            "/interaction/create",
+            tags=["Interaction"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+                status.HTTP_401_UNAUTHORIZED: {
+                    "description": "Error in authenticating user due to invalid token."
+                },
+            },
+        )
+        async def create_interaction(
+            token: str = Depends(self.auth.oauth2_scheme),
+        ):
+            """Create a new interaction."""
+            user_data = jwt.decode(
+                token, service_config.JWT_SECRET_KEY, algorithms=["HS256"]
+            )
+            response = await self.interaction_use_case.add_new_interaction(
+                Interaction(
+                    id=None,
+                    user_id=user_data["id"],
+                )
+            )
+            if response is None:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"message": "Failed to create interaction"},
+                )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"message": "Success, interaction created.", "interaction_id": response.id},
+            )
+
+        @self.rest_api_app.get(
+            "/interaction/fetch_all",
+            tags=["Interaction"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+                status.HTTP_401_UNAUTHORIZED: {
+                    "description": "Error in authenticating user due to invalid token."
+                },
+            },
+        )
+        async def fetch_all_interactions(
+            token: str = Depends(self.auth.oauth2_scheme),
+        ):
+            """Fetch all interactions for a user."""
+            user_data = jwt.decode(
+                token, service_config.JWT_SECRET_KEY, algorithms=["HS256"]
+            )
+            response = await self.interaction_use_case.retrieve_all(
+                user_id=user_data["id"],
+            )
+            if response is None:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"message": "Failed to fetch interactions"},
+                )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"interactions": json.dumps(response)},
+            )
+
+        @self.rest_api_app.post(
+            "/message/create",
+            tags=["Message"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+                status.HTTP_401_UNAUTHORIZED: {
+                    "description": "Error in authenticating user due to invalid token."
+                },
+            },
+        )
+        async def create_message(
+            form_data: CreateMessage,
+            token: str = Depends(self.auth.oauth2_scheme),
+        ):
+            """Create a new message in an interaction."""
+            user_data = jwt.decode(
+                token, service_config.JWT_SECRET_KEY, algorithms=["HS256"]
+            )
+            try:
+                response = await self.message_use_case.add_new_message(
+                    Message(
+                        id=None,
+                        interaction_id=form_data.interaction_id,
+                        role=RoleType.HUMAN.value,
+                        content=form_data.message,
+                    ),
+                    user_id=user_data["id"],
+                )
+            except InteractionNotFound as e:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Interaction not found, please create an interaction first. Error: {e}",
+                )
+            except UserNotAuthorized as e:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail=f"User not authorized. Error: {e}",
+                )
+            except Exception as e:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={
+                        "message": f"Failed to create and send a message. Error: {e}"
+                    },
+                )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"ai_response": response.content},
+            )
+
+        @self.rest_api_app.get(
+            "/message/fetch_all/{interaction_id}",
+            tags=["Message"],
+            responses={
+                status.HTTP_200_OK: {"description": "Success"},
+                status.HTTP_401_UNAUTHORIZED: {
+                    "description": "Error in authenticating user due to invalid token."
+                },
+            },
+        )
+        async def fetch_all_messages(
+            interaction_id: Union[int, str],
+            token: str = Depends(self.auth.oauth2_scheme),
+        ):
+            """Fetch all messages for an interaction."""
+            user_data = jwt.decode(
+                token, service_config.JWT_SECRET_KEY, algorithms=["HS256"]
+            )
+            try:
+                response = await self.message_use_case.retrieve_all(
+                    interaction_id=interaction_id,
+                    user_id=user_data["id"],
+                )
+            except InteractionNotFound as e:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Interaction not found, please create an interaction first. Error: {e}",
+                )
+            except UserNotAuthorized as e:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail=f"User not authorized. Error: {e}",
+                )
+            except Exception as e:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={
+                        "message": f"Failed to create and send a message. Error: {e}"
+                    },
+                )
+            if response is None:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"message": "Failed to fetch messages"},
+                )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"messages": json.dumps(response)},
             )
--- src/frameworks/api/test/test_api.py
+++ src/frameworks/api/test/test_api.py
@@ -3,31 +3,21 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from dependencies import container
-from src.adapters.authentication_provider import authentication
-from src.adapters.authentication_provider.authentication import AuthorizProviderConfig
-from src.config import authentication_provider_config
 from src import config_dependency_injection_test  # noqa
 from src.frameworks.api.entry_point import APIEndpoint
-from src.use_cases.interfaces.database import IDBRepository
+import json
 
 client = TestClient(container[APIEndpoint].rest_api_app)
 
 
 @pytest.fixture
 def request_header():
-    auth = authentication.Authenticate(
-        AuthorizProviderConfig(
-            server_url=authentication_provider_config.SERVER_URL,
-            client_id=authentication_provider_config.CLIENT_ID_AUTH_SERVICE,
-            realm_name=authentication_provider_config.REALM_NAME,
-            client_secret_key=authentication_provider_config.CLIENT_SECRET_KEY_AUTH_SERVICE,
-            token_url=authentication_provider_config.TOKEN_URL,
-        )
+    response = client.post(
+        "login", data={"username": "api@gmail.com", "password": "123456"}
     )
-    token = auth.login(
-        authentication_provider_config.TEST_USER,
-        authentication_provider_config.TEST_PASS,
-    )
+    token = response.json()
+    print(response)
+    print(token)
     return {"Authorization": token["token_type"] + " " + token["access_token"]}
 
 
@@ -38,13 +28,164 @@ def api_endpoint():
 
 @pytest.mark.order(11)
 @pytest.mark.asyncio
-async def test_1_get_api_docs():
+async def test_01_get_api_docs():
     response = client.get("/docs")
     assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.order(11)
 @pytest.mark.asyncio
-async def test_2_get_api_redoc():
+async def test_02_get_api_redoc():
     response = client.get("/redoc")
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_04_signup():
+    response = client.post(
+        "/signup",
+        data=json.dumps(
+            {
+                "name": "API Test User",
+                "email": "api@gmail.com",
+                "password": "123456",
+            }
+        ),
+    )
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert (
+        response_json["message"] == "Success, user created. Please login to continue."
+    )
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_03_login_check(request_header):
+    response = client.get("/service", headers=request_header)
+    assert response.status_code == status.HTTP_200_OK
+
+
+interaction_id = None
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_04_create_interaction(request_header):
+    response = client.post("/interaction/create", headers=request_header)
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["message"] == "Success, interaction created."
+    global interaction_id
+    interaction_id = response_json["interaction_id"]
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_05_create_message(request_header):
+    response = client.post(
+        "/message/create",
+        headers=request_header,
+        data=json.dumps(
+            {
+                "interaction_id": interaction_id,
+                "message": "Hello World",
+            }
+        ),
+    )
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["ai_response"] is not None
+    response = client.post(
+        "/message/create",
+        headers=request_header,
+        data=json.dumps(
+            {
+                "interaction_id": interaction_id,
+                "message": "Are you a bot?",
+            }
+        ),
+    )
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["ai_response"] is not None
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_05_create_message_failed(request_header):
+    response = client.post(
+        "/message/create",
+        headers=request_header,
+        data=json.dumps(
+            {
+                "interaction_id": 2,
+                "message": "Hello World",
+            }
+        ),
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    response = client.post(
+        "/message/create",
+        headers=request_header,
+        data=json.dumps(
+            {
+                "interaction_id": 10,
+                "message": "Hello World",
+            }
+        ),
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_06_fetch_all_messages(request_header):
+    response = client.get(
+        f"/message/fetch_all/{interaction_id}",
+        headers=request_header,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["messages"] is not None
+    data_json = json.loads(response_json["messages"])
+    assert len(data_json) == 2
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_07_fetch_all_messages_failed(request_header):
+    response = client.get(
+        "/message/fetch_all/2",
+        headers=request_header,
+    )
+    print(response.content)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    response = client.get(
+        "/message/fetch_all/10",
+        headers=request_header,
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.order(11)
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_08_fetch_all_interactions(request_header):
+    response = client.get(
+        "/interaction/fetch_all",
+        headers=request_header
+    )
+    assert response.status_code == status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["interactions"] is not None
+    data_json = json.loads(response_json["interactions"])
+    assert len(data_json) == 1
+    assert len(data_json[0]['messages']) == 2
