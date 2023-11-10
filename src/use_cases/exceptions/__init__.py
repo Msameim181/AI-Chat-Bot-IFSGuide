5d1c3c3eea1c495da4c56f83543394f4796f1c83
Mohammad Mahdi Samei <9259samei@gmail.com>
2023-11-10 16:48:28 +0000

    feature: exceptions added

---
+++ src/use_cases/exceptions/__init__.py
@@ -0,0 +1,30 @@
+
+class UserNotFound(Exception):
+    pass
+
+class UserAlreadyExists(Exception):
+    pass
+
+class UserNotCreated(Exception):
+    pass
+
+class UserNotUpdated(Exception):
+    pass
+
+class UserNotDeleted(Exception):
+    pass
+
+class UserNotLoggedIn(Exception):
+    pass
+
+class UserNotAuthorized(Exception):
+    pass
+
+class UserNotAuthenticated(Exception):
+    pass
+
+class InteractionNotFound(Exception):
+    pass
+
+class InformationIsNotValid(Exception):
+    pass
